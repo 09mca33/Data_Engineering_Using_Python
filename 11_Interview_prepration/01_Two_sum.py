4d14fb4596819e64a67c0e215b5ad5fe1ae9307d
Govind Singh Bora <g.singh.bora@gmail.com>
2025-10-05 15:58:59 +0000

    updated the code with better run time

--- 11_Interview_prepration/01_Two_sum.py
+++ 11_Interview_prepration/01_Two_sum.py
@@ -22,12 +22,20 @@ Output: [0,1]
 """
 
 
-def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+# def two_sum(nums, target):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
 
+
+def two_sum(nums, target):
+    num_map = {}
+    for item, number in enumerate(nums):
+        complement = target - number
+        if complement in num_map:
+            return [num_map[complement], item]
+        num_map[number] = item
 print(two_sum([2, 7, 11, 15], 9))
 print(two_sum([3, 2, 4], 6))
 print(two_sum([3, 3], 6))
