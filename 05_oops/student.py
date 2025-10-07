9d8445f40bdb1fd0575d634b07e55f5fd042f7df
Govind Singh Bora <g.singh.bora@gmail.com>
2025-10-07 21:46:33 +0530

    Opps class 2

--- 05_oops/student.py
+++ 05_oops/student.py
@@ -1,26 +1,48 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house} "
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self,house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,name):
+        if not name:
+            raise ValueError("Name is required")
+        self._name = name
+    
 def main():
-    # name = get_name()
-    # house = get_house()
-    name, house = get_student()
-    print(f"{name} from {house}")
-
-
-
-# def get_name():
-#     name = input("Enter your name: ")
-#     return name
-
-# def get_house():
-#     house = input("Enter your house: ")
-#     return house
+    student = get_student()
+    print(student)
+    # print(f'{student.name} from {student.house}')
+    # print(student.charm())
 
 
 def get_student():
-    name = input("Enter your name: ")
-    house = input("Enter your house: ")
-    return name, house
-
+    name = input("Name: ")
+    house = input("House: ")
+    try:
+        return Student(name, house)
+    except ValueError as e:
+        print(f"Error: {e}")
+        return None
 
 
 if __name__ == "__main__":
+    print("This is the main module")
     main()
