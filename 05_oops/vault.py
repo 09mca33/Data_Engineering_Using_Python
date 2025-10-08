c722dea67ca9855423648dfbd15a76e8450bc45a
Govind Singh Bora <g.singh.bora@gmail.com>
2025-10-08 21:47:44 +0530

    OOPS completed

---
+++ 05_oops/vault.py
@@ -0,0 +1,28 @@
+class Vault:
+    def __init__(self, galleons= 0, sickles = 0, knuts = 0): 
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+
+    def __str__(self) -> str:
+        return f"{self.galleons} galleons, {self.sickles} sickles, {self.knuts} knuts"
+
+    def __add__(self, other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+
+        return Vault(galleons, sickles,knuts)
+
+potter = Vault(100,50,25)
+print(potter)
+
+wesley = Vault(25,50,100)
+print(wesley)
+
+# galleons = potter.galleons + wesley.galleons
+# sickles = potter.sickles + wesley.sickles
+# knuts = potter.knuts + wesley.knuts
+
+# total = Vault(galleons, sickles, knuts)        
+print(potter + wesley)
